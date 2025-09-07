521963e41aeee329c7b38b9e9d688f475d27d3af
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-09-08 03:54:21 +0500

    Create Chain.py

---
+++ ReflectionAgent/Chain.py
@@ -0,0 +1,41 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+from dotenv import load_dotenv
+# ---------------------------------------------------
+# 1. Load environment variables from .env
+# ---------------------------------------------------
+load_dotenv()
+
+generation_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a twitter techie influencer assistant tasked with writing excellent twitter posts."
+            " Generate the best twitter post possible for the user's request."
+            " If the user provides critique, respond with a revised version of your previous attempts.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+reflection_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a viral twitter influencer grading a tweet. Generate critique and recommendations for the user's tweet."
+            "Always provide detailed recommendations, including requests for length, virality, style, etc.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+# Get Google API key from environment
+google_api_key = os.getenv("GOOGLE_API_KEY")
+llm = ChatGoogleGenerativeAI(
+    model='gemini-1.5-flash',
+    google_api_key=google_api_key
+)
+
+
+generation_chain = generation_prompt | llm
+reflection_chain = reflection_prompt | llm
